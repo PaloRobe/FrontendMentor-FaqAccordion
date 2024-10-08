5b052f9fca09e639b780e9de4255bb005f64dffa
r.palombelli <r.palombelli@eustema.it>
2024-10-08 19:50:43 +0200

    Commit code solution

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        White: "hsl(0, 0%, 100%)",
+        LightPink:"hsl(275, 100%, 97%)",
+        GrayishPurple:"hsl(292, 16%, 49%)",
+        DarkPurple:"hsl(292, 42%, 14%)"
+      },
+      fontFamily:{
+        WorkSans: ["Work Sans", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
